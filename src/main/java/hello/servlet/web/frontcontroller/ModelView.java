98af10b0d2429fad153b2c14f70d57b95cd8dc24
skahn1215 <d1215@naver.com>
2021-10-23 16:06:03 +0900

    upgrade v2 to v3

---
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,27 @@
+package hello.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelView {
+    private String viewName;
+
+    // jsp 에서 쓸수 있도록 데이터를 담아 둔다.
+    private Map<String, Object> model = new HashMap<>();
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void setModel(Map<String, Object> model) {
+        this.model = model;
+    }
+}
--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -1,8 +1,5 @@
 package hello.servlet.web.frontcontroller.v2;
-
-
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v1.ControllerV1;
 import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
 import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
 import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
