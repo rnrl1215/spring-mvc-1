4ef54ae79271b10e2d7ef2bda7b5003cbc236971
skahn1215 <d1215@naver.com>
2021-10-30 01:16:16 +0900

    added controller adapter

--- src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -5,7 +5,6 @@ import hello.servlet.web.frontcontroller.ModelView;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.rowset.serial.SerialException;
 import java.io.IOException;
 
 public interface MyHandlerAdapter {
